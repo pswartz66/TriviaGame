b452d6ae6b466e3df2071cd1e8e6a5885301f364
Phil Swartz <pswartz.sju@gmail.com>
2019-09-08 13:15:44 -0400

    working on combining the answer arrays into one to be used for randomizing order of questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,9 +6,9 @@ var queryURL = "https://opentdb.com/api.php?amount=10&category=18&difficulty=med
 var questions = [];
 var answers = [];
 var incorrectAnswers = [];
-// var API_results = [];
 
 var calledAPI = false;
+
 
 $.ajax({
     url: queryURL,
@@ -20,37 +20,50 @@ $.ajax({
 
         questions.push(response.results[i].question);
         
-        answers.push(response.results[i].correct_answer);
-
-        
+        answers.push(response.results[i].correct_answer); 
         
     }
 
-    for (let j = 0; response.results[j].incorrect_answers.length; j++){
+    for (let j = 0; j < 10; j++){
+
         incorrectAnswers.push(response.results[j].incorrect_answers[0]);
+        
         incorrectAnswers.push(response.results[j].incorrect_answers[1]);
+        
         incorrectAnswers.push(response.results[j].incorrect_answers[2]);
         
     }
-
-    /* API_results.push(response.results[0]);
-    API_results.push(response.results[1]);
-    API_results.push(response.results[2]);
-    API_results.push(response.results[3]);
-    API_results.push(response.results[4]);
-    API_results.push(response.results[5]);
-    API_results.push(response.results[6]);
-    API_results.push(response.results[7]);
-    API_results.push(response.results[8]);
-    API_results.push(response.results[9]);
-    API_results.push(response.results[10]); */
+    
 
     calledAPI = true;
+
 });
 
 console.log(questions);
 console.log(answers);
 console.log(incorrectAnswers);
+
+// map 4 answers per question into it's own array
+// will allow for easier answer array randomization
+var mappedAnswers = [];
+var x = 3;
+
+console.log(questions.length)
+
+for (var k = 0; k < questions.length; k++) {
+    // 0 - 0, 1, 2
+    // 1 - 3, 4, 5 
+    // 2 - 6, 7, 8
+
+
+    console.log(questions[k]);
+
+    /* mappedAnswers.push(answers[p])
+    
+    x += 3;
+
+    console.log(mappedAnswers); */
+};
 
 
 /* ------------------------------------ */
@@ -58,41 +71,63 @@ console.log(incorrectAnswers);
 
 function generateQuestionAndAnswer(){
 
+
     // PUT THIS INSIDE A FUNCTION THAT GENERATES A QUESTION AND RANDOMIZES THE ANSWER ORDER
     $("#time-remaining").text("Time Remaining: " + count);
 
 
+
     $('#time-remaining').append('<tbody></tbody>')
     $('#time-remaining > tbody:last-child').append('<tr></tr>');
+    $('#time-remaining > tbody > tr').addClass('question');
     $('#time-remaining > tbody > tr:last-child').text(questions[0]);
 
     $('#time-remaining > tbody:last-child').append('<br></brr>');
 
     $('#time-remaining > tbody:last-child').append('<tr></tr>');
+    $('#time-remaining > tbody > tr').attr('id', 'answer');
     $('#time-remaining > tbody > tr:last-child').text(answers[0]);
 
     $('#time-remaining > tbody:last-child').append('<tr></tr>');
+    $('#time-remaining > tbody > tr').attr('id', 'answer');
     $('#time-remaining > tbody > tr:last-child').text(incorrectAnswers[0]);
 
     $('#time-remaining > tbody:last-child').append('<tr></tr>');
+    $('#time-remaining > tbody > tr').attr('id', 'answer');
     $('#time-remaining > tbody > tr:last-child').text(incorrectAnswers[1]);
 
     $('#time-remaining > tbody:last-child').append('<tr></tr>');
+    $('#time-remaining > tbody > tr').attr('id', 'answer');
     $('#time-remaining > tbody > tr:last-child').text(incorrectAnswers[2]);
+
+
+
+    /* $('#time-remaining > tbody > tr').hover(function(){
+        $(this).css("background-color", "blue");
+    }); */
+
 
 };
 
+
+// function to clear the Q & A section
+function clearQuestionAndAnswer(){
+
+    $('#time-remaining > tbody').empty();
+
+}
 
 
 // set isStartClicked to determine if the start button was clicked
 var isStartClicked = false;
 
 // counter variable per question
-var count = 20;
+var count = 5;
 
 
 // when start button is clicked run this code
 $("#start-button").on("click", function () {
+
     isStartClicked = true;
 
     // when start button is clicked, hide it and begin counter
@@ -122,7 +157,6 @@ function countDown() {
         generateQuestionAndAnswer();
 
 
-
         // decrement by 1
         count--;
 
@@ -132,6 +166,15 @@ function countDown() {
     if (count === -1) {
         stop();
         console.log("times up")
+
+        // log the users guess here
+
+
+        
+
+
+        // clear the question and answer field
+        // clearQuestionAndAnswer();
         
     }
 
