7dacf9edf1f8a4e3e1aa19be92e1ff3ceb4bdcc6
Phil Swartz <pswartz.sju@gmail.com>
2019-09-08 15:42:51 -0400

    api call now adds question and answer arrays, game resets a new question after 5 seconds, working on adding in user clicks answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,7 @@ var incorrectAnswers = [];
 var calledAPI = false;
 
 
+
 $.ajax({
     url: queryURL,
     method: "GET"
@@ -20,85 +21,73 @@ $.ajax({
 
         questions.push(response.results[i].question);
         
-        answers.push(response.results[i].correct_answer); 
+        var random = Math.floor(Math.random() * 3)
+
+        var randNum = [0, 1, 2];
+
+        answers.push(response.results[i].correct_answer,
+                    response.results[i].incorrect_answers[0],
+                    response.results[i].incorrect_answers[1],
+                    response.results[i].incorrect_answers[2]
+                    ); 
         
     }
 
-    for (let j = 0; j < 10; j++){
 
-        incorrectAnswers.push(response.results[j].incorrect_answers[0]);
-        
-        incorrectAnswers.push(response.results[j].incorrect_answers[1]);
-        
-        incorrectAnswers.push(response.results[j].incorrect_answers[2]);
-        
+    for (let i = 0; i < answers.length; i++) {
+        console.log(answers[i]);
+
     }
+
     
-
     calledAPI = true;
 
 });
 
+
+
 console.log(questions);
 console.log(answers);
-console.log(incorrectAnswers);
-
-// map 4 answers per question into it's own array
-// will allow for easier answer array randomization
-var mappedAnswers = [];
-var x = 3;
-
-console.log(questions.length)
-
-for (var k = 0; k < questions.length; k++) {
-    // 0 - 0, 1, 2
-    // 1 - 3, 4, 5 
-    // 2 - 6, 7, 8
 
 
-    console.log(questions[k]);
-
-    /* mappedAnswers.push(answers[p])
-    
-    x += 3;
-
-    console.log(mappedAnswers); */
-};
 
 
 /* ------------------------------------ */
 
+var answerCounter = 0;
+var questionCounter = 0;
+var questionCount = 10;
 
 function generateQuestionAndAnswer(){
-
+     
+    
 
     // PUT THIS INSIDE A FUNCTION THAT GENERATES A QUESTION AND RANDOMIZES THE ANSWER ORDER
     $("#time-remaining").text("Time Remaining: " + count);
 
 
-
     $('#time-remaining').append('<tbody></tbody>')
     $('#time-remaining > tbody:last-child').append('<tr></tr>');
     $('#time-remaining > tbody > tr').addClass('question');
-    $('#time-remaining > tbody > tr:last-child').text(questions[0]);
+    $('#time-remaining > tbody > tr:last-child').text(questions[questionCounter]);
 
     $('#time-remaining > tbody:last-child').append('<br></brr>');
 
     $('#time-remaining > tbody:last-child').append('<tr></tr>');
     $('#time-remaining > tbody > tr').attr('id', 'answer');
-    $('#time-remaining > tbody > tr:last-child').text(answers[0]);
+    $('#time-remaining > tbody > tr:last-child').text(answers[answerCounter]);
 
     $('#time-remaining > tbody:last-child').append('<tr></tr>');
     $('#time-remaining > tbody > tr').attr('id', 'answer');
-    $('#time-remaining > tbody > tr:last-child').text(incorrectAnswers[0]);
+    $('#time-remaining > tbody > tr:last-child').text(answers[answerCounter + 1]);
 
     $('#time-remaining > tbody:last-child').append('<tr></tr>');
     $('#time-remaining > tbody > tr').attr('id', 'answer');
-    $('#time-remaining > tbody > tr:last-child').text(incorrectAnswers[1]);
+    $('#time-remaining > tbody > tr:last-child').text(answers[answerCounter + 2]);
 
     $('#time-remaining > tbody:last-child').append('<tr></tr>');
     $('#time-remaining > tbody > tr').attr('id', 'answer');
-    $('#time-remaining > tbody > tr:last-child').text(incorrectAnswers[2]);
+    $('#time-remaining > tbody > tr:last-child').text(answers[answerCounter + 3]);
 
 
 
@@ -106,6 +95,7 @@ function generateQuestionAndAnswer(){
         $(this).css("background-color", "blue");
     }); */
 
+    
 
 };
 
@@ -116,6 +106,8 @@ function clearQuestionAndAnswer(){
     $('#time-remaining > tbody').empty();
 
 }
+
+
 
 
 // set isStartClicked to determine if the start button was clicked
@@ -139,6 +131,7 @@ $("#start-button").on("click", function () {
 
 
 
+
 // function to begin the counting for the start of the first question
 function start() {
 
@@ -155,13 +148,25 @@ function countDown() {
         
         // call generate question and answer function
         generateQuestionAndAnswer();
+        
 
+        // decrement by 1
+        count--;
+
+
+    }
+    else {
+
+        // call generate question and answer function
+        generateQuestionAndAnswer();
+        
 
         // decrement by 1
         count--;
 
     }
 
+    
 
     if (count === -1) {
         stop();
@@ -170,11 +175,19 @@ function countDown() {
         // log the users guess here
 
 
-        
-
-
         // clear the question and answer field
-        // clearQuestionAndAnswer();
+        clearQuestionAndAnswer();
+
+        // set count back to 5
+        count = 5;
+
+
+        answerCounter += 4;
+        questionCounter += 1;
+        generateQuestionAndAnswer();
+
+        start();
+        
         
     }
 
@@ -185,6 +198,8 @@ function stop() {
 
     // clear the time interval
     clearInterval(intervalId);
+
+    
 
     // set start button click back to zero
     isStartClicked = false;
